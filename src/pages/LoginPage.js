9fbbb6adbc314d555f7ba72e22517b85fa70351b
Mohan_krishna <mohankrishnamadanapu@gmail.com>
2022-02-04 10:45:54 +0530

    login page comments

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,6 +5,7 @@ import LockIcon from '@mui/icons-material/Lock';
 import * as Yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+//comments in login page
 
 const LoginPage = () => {
 
